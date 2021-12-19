19e4e08599e344e96e0a119ba5e3531eb73b4701
Justin Eloff <ekpoepol@gmail.com>
2021-12-19 03:56:45 +0100

    Remove console logs

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -2,16 +2,9 @@ import {writable} from 'svelte/store';
 
 export const menuItems = writable({});
 
-menuItems.subscribe(items => {
-  console.log(items);
-  // return items;
-})
-
 menuItems.register = (name, config) => {
   menuItems.update(items => {
-    console.log(name, config, items);
     if (items[name]) {
-      console.warn(`Menu with name ${name} already exists.`)
       return items;
     }
 
