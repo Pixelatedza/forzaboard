b4986a86544d24b1456a9ca6e2c847c4ea5a5caf
justin.eloff <justine@bbdsoftware.com>
2022-01-08 18:20:12 +0100

    Update leaderboard and allow users to delete records

--- src/api/records.js
+++ src/api/records.js
@@ -1,6 +1,7 @@
-import {get, post} from 'common';
+import {get, post, remove} from 'common';
 
-export const getRecords = (options) => get('records', options);
+export const getRecords = options => get('records', options);
 export const getRecord = id => get(`records/${id}`);
-export const createRecord = (data) => post('records', data);
+export const createRecord = data => post('records', data);
+export const removeRecord = id => remove(`records/${id}`);
 
--- src/components/_docs/Leaderboard.svelte
+++ src/components/_docs/Leaderboard.svelte
@@ -1,24 +1,43 @@
 <script>
   import Leaderboard from 'components/Leaderboard.svelte'
 
-  let records = Array(100).fill(
+  let records = [
     {
-      "user": "ForzaboardUser1",
+      "user": {
+        "uuid": "8fd1a9d2-420a-4fb2-b038-394d9b8e757a",
+        "username": "other-user"
+      },
       "car": "Test car",
-      "uuid": "912fdad6-65fb-48c0-a3d2-f51658cdd387",
-      "value": 32323,
+      "uuid": "aa0ab9f6-7bc8-4a18-a54e-66472d25e8c8",
+      "value": 2324,
       "share_code": null,
-      "pi": 349,
+      "pi": null,
       "video": null,
       "platform": null,
-      "created": "2021-12-27T19:10:41.337301Z",
-      "event": "ad688fa1-ed02-41ad-9b2d-e886da9607aa"
+      "created": "2022-01-08T16:33:49.025210Z",
+      "event": "b074c2a5-9b10-4900-94a1-9b254152fac6"
+    },
+    {
+      "user": {
+        "uuid": "5b1e950a-7235-4f44-972a-a7d345c2f720",
+        "username": "admin"
+      },
+      "car": "Test car",
+      "uuid": "671464ea-be8a-4544-ad1c-3dfcddc60e80",
+      "value": 1312312,
+      "share_code": null,
+      "pi": null,
+      "video": null,
+      "platform": null,
+      "created": "2022-01-08T17:18:34.702108Z",
+      "event": "ca6de71b-ec18-4058-b018-c1e6ecea6f14"
     }
-  );
+  ];
 </script>
 
 <style>
   div {
+      display: flex;
       max-height: 100vh;
       overflow: hidden;
   }
@@ -26,7 +45,7 @@
 
 <div>
   <Leaderboard
-    records={records}
+    bind:records={records}
     location={{
         "kind": 'danger_sign',
         "uuid": "b290bc54-ab99-4980-9509-c34b72713359",
